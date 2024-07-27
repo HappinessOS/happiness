5dc4dc26c36deb83f43b216640b3f9df7d2a9d4c
morbidly-obese-thanh <morbidly.obese.thanh@outlook.com>
2024-07-27 10:57:20 +0200

    uptime: Use int instead of time_t.

--- uptime.c
+++ uptime.c
@@ -1,6 +1,5 @@
 #include <stdint.h>
 #include <stdio.h>
-#include <time.h>
 
 int main(void) {
   FILE *stream = fopen("/proc/uptime", "r");
@@ -8,17 +7,15 @@ int main(void) {
     char str[BUFSIZ];
     fgets(str, BUFSIZ, stream);
 
-    double upsecs;
-    sscanf(str, "%lf", &upsecs);
+    double uptime_secs;
+    sscanf(str, "%lf", &uptime_secs);
 
-    const time_t seconds = (time_t)upsecs;
-
-    const intmax_t updays = seconds / 86400;
-    const int uphours = seconds % 86400 / 3600;
-    const int upmins = seconds % 86400 % 3600 / 60;
+    const int updays = (int)uptime_secs / 86400;
+    const int uphours = (int)uptime_secs % 86400 / 3600;
+    const int upmins = (int)uptime_secs % 86400 % 3600 / 60;
 
     if (updays > 0) {
-      printf("Up %jd day%s %2d:%02d\n", updays, updays > 1 ? "s" : "", uphours,
+      printf("Up %d day%s %2d:%02d\n", updays, updays > 1 ? "s" : "", uphours,
              upmins);
     } else if (uphours > 0) {
       printf("Up %2d:%02d\n", uphours, upmins);
