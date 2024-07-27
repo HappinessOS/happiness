746844d4c45f3e1fd7f9947e40d2f3b6b223103a
morbidly-obese-thanh <morbidly.obese.thanh@outlook.com>
2024-07-27 04:59:25 +0200

    uptime: Format time.

--- uptime.c
+++ uptime.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(void) {
   FILE *stream = fopen("/proc/uptime", "r");
@@ -6,10 +7,17 @@ int main(void) {
     char str[BUFSIZ];
     fgets(str, BUFSIZ, stream);
 
-    double seconds;
-    sscanf(str, "%lf", &seconds);
+    double upsecs;
+    sscanf(str, "%lf", &upsecs);
 
-    printf("up %.2f seconds\n", seconds);
+    const time_t seconds = (time_t)upsecs;
+    printf("up %ld day%s, ", seconds / 86400, seconds / 86400 > 0 ? "s" : "");
+    printf("%ld hour%s, ", seconds % 86400 / 3600,
+           seconds % 86400 / 3600 > 0 ? "s" : "");
+    printf("%ld minute%s, ", seconds % 86400 % 3600 / 60,
+           seconds % 86400 % 3600 / 60 > 0 ? "s" : "");
+    printf("%ld second%s\n", seconds % 86400 % 3600 % 60,
+           seconds % 86400 % 3600 % 60 > 0 ? "s" : "");
   }
 
   fclose(stream);
