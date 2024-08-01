6099501c47ff7968fd2ed05d9179b3924be4c37c
morbidly-obese-thanh <morbidly.obese.thanh@outlook.com>
2024-08-01 05:36:00 +0200

    uptime: Add -p flag.

--- uptime.c
+++ uptime.c
@@ -1,7 +1,8 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <time.h>
 
-int main(void) {
+int main(int argc, char **argv) {
   FILE *stream = fopen("/proc/uptime", "r");
   if (stream != nullptr) {
     double uptime_secs;
@@ -12,11 +13,29 @@ int main(void) {
     const struct tm *tm = localtime(&timer);
 
     // FIXME: Allow setting via command-line argument.
+    int pretty = 0;
+    const struct option longopts[] = {
+        {"pretty", no_argument, nullptr, 'p'},
+    };
+
+    int c;
+    while ((c = getopt_long(argc, argv, "p", longopts, nullptr)) != -1) {
+      switch (c) {
+      case 'p':
+        pretty = 1;
+        break;
+      default:;
+      }
+    }
 
     char uptime_str[100];
     unsigned len = 0;
-    len += snprintf(uptime_str + len, 100 - len, " %02d:%02d:%02d up ",
-                    tm->tm_hour, tm->tm_min, tm->tm_sec);
+    if (pretty) {
+      len += snprintf(uptime_str + len, 100 - len, "up ");
+    } else {
+      len += snprintf(uptime_str + len, 100 - len, " %02d:%02d:%02d up ",
+                      tm->tm_hour, tm->tm_min, tm->tm_sec);
+    }
 
     const int updays = (int)uptime_secs / 86400;
     const int uphours = (int)uptime_secs % 86400 / 3600;
@@ -27,10 +46,21 @@ int main(void) {
                       updays > 1 ? "s" : "");
     }
 
-    if (uphours > 0) {
-      len += snprintf(uptime_str + len, 100 - len, "%2d:%02d", uphours, upmins);
+    if (pretty) {
+      if (uphours > 0) {
+        len += snprintf(uptime_str + len, 100 - len, "%d hour%s, ", uphours,
+                        uphours > 1 ? "s" : "");
+      }
+
+      len += snprintf(uptime_str + len, 100 - len, "%d minute%s", upmins,
+                      upmins > 1 ? "s" : "");
     } else {
-      len += snprintf(uptime_str + len, 100 - len, "%d min", upmins);
+      if (uphours > 0) {
+        len +=
+            snprintf(uptime_str + len, 100 - len, "%2d:%02d", uphours, upmins);
+      } else {
+        len += snprintf(uptime_str + len, 100 - len, "%d min", upmins);
+      }
     }
 
     printf("%s\n", uptime_str);
